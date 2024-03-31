233073634484ef960aa66a68b01f5779dca953c7
AppleLee0423 <ld210@naver.com>
2024-03-31 13:17:43 +0900

    init commit

---
+++ src/main/java/com/agencypro02/agencypro02/AgencyPro02Application.java
@@ -0,0 +1,13 @@
+package com.agencypro02.agencypro02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyPro02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyPro02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/agencypro02/agencypro02/AgencyPro02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencypro02.agencypro02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyPro02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
